cf12c2f6eb8c624f707a6efe05a1dff329c298ec
xtreak <tir.karthi@gmail.com>
2018-04-26 14:34:14 +0530

    Update docs and format examples with lein cljfmt

--- examples/class-scheduling/main.clj
+++ examples/class-scheduling/main.clj
@@ -7,17 +7,15 @@
   (:require [clj-foundationdb.core :refer :all]
             [clj-foundationdb.utils :refer :all]))
 
-
 (def times '("2:00", "3:00", "4:00",
-             "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00",
-             "14:00", "15:00", "16:00", "17:00", "18:00", "19:00"))
+                     "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00",
+                     "14:00", "15:00", "16:00", "17:00", "18:00", "19:00"))
 
 (def types '("chem", "bio", "cs",
-             "geometry", "calc", "alg", "film", "music", "art", "dance"))
+                     "geometry", "calc", "alg", "film", "music", "art", "dance"))
 
 (def levels '("intro", "for dummies",
-              "remedial", "101", "201", "301", "mastery", "lab", "seminar"))
-
+                       "remedial", "101", "201", "301", "mastery", "lab", "seminar"))
 
 (defn get-class-names
   []
@@ -25,7 +23,6 @@
         type  types
         time  times]
     (clojure.string/join " " [level type time])))
-
 
 (defn init-db
   []
@@ -36,13 +33,11 @@
       (.run db (clear-range attendance-records))
       (.run db (clear-range class-records)))))
 
-
 (defn add-class
   [classes]
   (let [fd            (. FDB selectAPIVersion 510)]
     (with-open [db (.open fd)]
       (.run db (set-keys (map #(vector "class" %1) classes) "100")))))
-
 
 (defn current-class-attendance
   [class-name]
@@ -51,7 +46,6 @@
     (with-open [db (.open fd)]
       (if-let [value (.run db (get-val record))]
         (Integer/parseInt value)))))
-
 
 (defn signup-class
   [student class-name]
@@ -69,14 +63,12 @@
           (.run db (set-val class (str (dec (int seats)))))
           "Unable to sign up")))))
 
-
 (defn drop-class
   [student class-name]
   (let [fd     (. FDB selectAPIVersion 510)
         record (.pack (Tuple/from (to-array ["attends" student class-name])))]
     (with-open [db (.open fd)]
       (.run db (clear-val record)))))
-
 
 (defn available-classes
   []
